a830d6f4666b6808355dcaf2ab2c7f8543c5d7b1
apetrovskiy <apetrovskiy@hotmail.com>
2016-05-05 08:39:34 +0300

    .

---
+++ testApp002/testApp002/Types/ChangeDetail.py
@@ -0,0 +1,6 @@
+class ChangeDetail(object):
+    """description of class"""
+    def __init__(self, *args, **kwargs):
+        return super().__init__(*args, **kwargs)
+
+
---
+++ testApp002/testApp002/Types/StandardChange.py
@@ -0,0 +1,6 @@
+class StandardChange(object):
+    """description of class"""
+    def __init__(self, *args, **kwargs):
+        return super().__init__(*args, **kwargs)
+
+
