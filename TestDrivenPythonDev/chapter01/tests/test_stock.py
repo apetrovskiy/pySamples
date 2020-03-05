edb4c297b22df703e16d81c979968fe861392e5a
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-03-05 16:07:08 +0300

    test-driven-python-dev ch02 002

--- TestDrivenPythonDev/chapter01/tests/test_stock.py
+++ TestDrivenPythonDev/chapter01/tests/test_stock.py
@@ -13,5 +13,13 @@ class StockTest(TestCase):
         We will be using the `datetime` module for the timestamp
         """
         goog = Stock("GOOG")
-        goog.update(datetime(2014, 2, 12), price = 10)
+        goog.update(datetime(2014, 2, 12), price=10)
         self.assertEqual(10, goog.price)
+
+    def test_negative_price_should_throw_ValueError(self):
+        goog = Stock("GOOG")
+        try:
+            goog.update(datetime(2014, 2, 13), -1)
+        except ValueError:
+            return
+        self.fail("ValueError was not raised")
