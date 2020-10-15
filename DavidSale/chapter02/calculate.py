fc96ba0144eac4e8c32fac20c783bfebc968f8ee
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-10-15 16:36:57 +0300

    .

--- DavidSale/chapter02/calculate.py
+++ DavidSale/chapter02/calculate.py
@@ -1,9 +1,13 @@
 class Calculate():
     def add(self, x, y):
-        return x + y
+        if type(x) == int and type(y) == int:
+            return x + y
+        else:
+            raise TypeError("Invalid type: {} and {}".format(type(x), type(y)))
 
 
 if __name__ == '__main__':
     calc = Calculate()
     result = calc.add(2, 2)
     print(result)
+    print(calc.add("Hello", "World"))
