58bebe2f64c35bf92aba81a86735c7fef1b357b9
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2020-10-15 12:47:04 +0300

    Update testApp003.py

--- testApp003/testApp003/testApp003.py
+++ testApp003/testApp003/testApp003.py
@@ -8,6 +8,7 @@ def make_dot_string(x):
 assert make_dot_string(90) == '          o'
 assert make_dot_string(180) == 'o'
 
+
 def main():
     for i in range(10000000):
         s = make_dot_string(i)
