63859610209bc26ebc64cd9ee3acae17011277e8
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-02-03 11:26:53 +0300

    finished

--- testPyYaml/load_data.py
+++ testPyYaml/load_data.py
@@ -1,10 +1,14 @@
 import yaml
-from model.config import Config
+from model.config.data import Config
 
 
-config = Config()
+config = Config(111, "sss")
 
-with open('data.yaml') as f:
-    result = yaml.safe_load(f)
+# with open('data.yaml') as f:
+#     result = yaml.safe_load(f)
+stream = open('data.yaml', 'r')
+result = yaml.load(stream, Loader=yaml.Loader)
 
 print(result)
+print(result.id)
+print(result.path)
--- testPyYaml/model/config/data.py
+++ testPyYaml/model/config/data.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 
 # @dataclass
 class Config(yaml.YAMLObject):
-    # yaml_tag = u'!Config'
+    yaml_tag = 'tag:yaml.org,2002:model.config.data.Config'
     def __init__(self, id: int, path: str):
         self.id = id
         self.path = path
