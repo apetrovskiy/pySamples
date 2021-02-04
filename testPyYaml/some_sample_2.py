cacc6e9d06d6095258b4082ad2d396f89da4c3f5
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-02-04 18:58:21 +0300

    Update some_sample_2.py

--- testPyYaml/some_sample_2.py
+++ testPyYaml/some_sample_2.py
@@ -4,19 +4,20 @@ from collections import Counter
 from typing import List, Tuple
 
 
-def grouper(input:str)->List[Tuple[str,int]]:
+def grouper(input: str) -> List[Tuple[str, int]]:
     number_of_items = 1
-    def calc(index:int):
+
+    def calc(index: int):
         nonlocal number_of_items
         if index+1 < len(input):
             if input[index] != input[index+1]:
-                result = (input[index],number_of_items)
+                result = (input[index], number_of_items)
                 number_of_items = 1
                 return result
             else:
                 number_of_items += 1
         else:
-           return (input[index],number_of_items)
+            return (input[index], number_of_items)
     return [x for x in [calc(index) for index in range(len(input))] if x is not None]
 
 
