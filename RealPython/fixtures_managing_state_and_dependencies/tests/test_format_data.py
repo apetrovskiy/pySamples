2de7d37ac735515b1550912dc85cbba38fb363b7
Alexander Petrovskiy <uiautomation@hotmail.com>
2023-10-14 21:01:16 +0300

    the initial samples (#19)

---
+++ RealPython/fixtures_managing_state_and_dependencies/tests/test_format_data.py
@@ -0,0 +1,42 @@
+# test_format_data.py
+
+
+import pytest
+
+from fixtures_managing_state_and_dependencies.src.format_data import (
+    format_data_for_display,
+    format_data_for_excel,
+)
+
+
+@pytest.fixture
+def example_people_data():
+    return [
+        {
+            "given_name": "Alfonsa",
+            "family_name": "Ruiz",
+            "title": "Senior Software Engineer",
+        },
+        {
+            "given_name": "Sayid",
+            "family_name": "Khan",
+            "title": "Project Manager",
+        },
+    ]
+
+
+def test_format_data_for_display(example_people_data):
+    assert format_data_for_display(example_people_data) == [
+        "Alfonsa Ruiz: Senior Software Engineer",
+        "Sayid Khan: Project Manager",
+    ]
+
+
+def test_format_data_for_excel(example_people_data):
+    assert (
+        format_data_for_excel(example_people_data)
+        == """given,family,title
+Alfonsa,Ruiz,Senior Software Engineer
+Sayid,Khan,Project Manager
+"""
+    )
