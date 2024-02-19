8e73cb19fb2dd03719b1f481e026052d545ac3a3
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2024-02-20 00:47:05 +0300

    formatted

--- PythonToday/pdf_to_mp3/main.py
+++ PythonToday/pdf_to_mp3/main.py
@@ -2,31 +2,36 @@ from gtts import gTTS
 import pdfplumber
 from pathlib import Path
 
-TEXT1='text1.txt'
-TEXT2='text2.txt'
+TEXT1 = "text1.txt"
+TEXT2 = "text2.txt"
 
-def pdf_to_mp3(file_path='test.pdf',language='en'):
-    if Path(file_path).is_file() and Path(file_path).suffix=='.pdf':
+
+def pdf_to_mp3(file_path="test.pdf", language="en"):
+    if Path(file_path).is_file() and Path(file_path).suffix == ".pdf":
         # return 'File exists!'
 
-        with pdfplumber.PDF(open(file=file_path,mode='rb')) as pdf:
-            pages =  [page.extract_text() for page in pdf.pages]
-        text = ''.join(pages)
+        with pdfplumber.PDF(open(file=file_path, mode="rb")) as pdf:
+            pages = [page.extract_text() for page in pdf.pages]
+        text = "".join(pages)
 
-        with open(TEXT1,'w') as file:
+        with open(TEXT1, "w") as file:
             file.write(text)
 
-        text=text.replace('\n','')
+        text = text.replace("\n", "")
 
-        with open(TEXT2,'w') as file:
+        with open(TEXT2, "w") as file:
             file.write(text)
     else:
-        return 'File does not exist, check the file path!'
+        return "File does not exist, check the file path!"
+
 
 def main():
     print(pdf_to_mp3(file_path="./2.pdf"))
 
-if __name__ == '__main__':
-    if Path(TEXT1).exists(): Path(TEXT1).unlink()
-    if Path(TEXT2).exists(): Path(TEXT2).unlink()
+
+if __name__ == "__main__":
+    if Path(TEXT1).exists():
+        Path(TEXT1).unlink()
+    if Path(TEXT2).exists():
+        Path(TEXT2).unlink()
     main()
