7c07b79909faa23b39b966ffa6b67a62938c5673
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2024-02-20 03:00:30 +0300

    almost done but formatting

--- PythonToday/pdf_to_mp3/main.py
+++ PythonToday/pdf_to_mp3/main.py
@@ -1,4 +1,5 @@
 from gtts import gTTS
+from art import tprint
 import pdfplumber
 from pathlib import Path
 
@@ -9,24 +10,38 @@ TEXT2 = "text2.txt"
 def pdf_to_mp3(file_path="test.pdf", language="en"):
     if Path(file_path).is_file() and Path(file_path).suffix == ".pdf":
         # return 'File exists!'
+        print(f'[+] Original file: {Path(file_path).name}')
+        print('[+] Processing...')
 
         with pdfplumber.PDF(open(file=file_path, mode="rb")) as pdf:
             pages = [page.extract_text() for page in pdf.pages]
         text = "".join(pages)
 
+        # temporatily
         with open(TEXT1, "w") as file:
             file.write(text)
 
         text = text.replace("\n", "")
 
+        # temporatily
         with open(TEXT2, "w") as file:
             file.write(text)
+
+        my_audio = gTTS(text=text, lang=language)
+        file_name = Path(file_path).stem
+        my_audio.save(f"{file_name}.mp3")
+
+        return f"[+] {file_name}.mp3 saved successfully\n...Have a good day"
+
     else:
         return "File does not exist, check the file path!"
 
 
 def main():
-    print(pdf_to_mp3(file_path="./2.pdf"))
+    tprint('PDF>>TO>>MP3',font='')
+    file_path=input("\nEnter a file's path: ")
+    language=input("Choose language, for example, 'en' or 'ru'")
+    print(pdf_to_mp3(file_path=("./1.pdf" if file_path == '' else file_path),language="en" if language =='' else language))
 
 
 if __name__ == "__main__":
