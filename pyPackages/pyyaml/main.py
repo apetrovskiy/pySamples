175150a15fe4c5d419fc0e82630c4c041595cd6a
Aleksandr Petrovskiy <uiautomation@hotmail.com>
2024-03-02 23:39:52 +0300

    now it's working

--- pyPackages/pyyaml/main.py
+++ pyPackages/pyyaml/main.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 import yaml
-
+from configuration import Configuration
 from src.core.config.config_loader import load_config
 from src.model.config.config import Config
 
@@ -12,16 +12,31 @@ def load_config_2(path: str) -> Config:
         return yaml.load(file, Loader=yaml.Loader)
 
 
+def load_config_3(path: str) -> Configuration:
+    with open(path, "r") as file:
+        return yaml.load(file, Loader=yaml.Loader)
+
+
 config01 = load_config()
 pprint(config01)
+print(config01.postgres_config.database)
+print(
+    "===================================================================================================="
+)
 config02 = load_config_2(CONFIG_FILE_NAME)
 pprint(config02)
-config03 = load_config_2("test.yml")
+print(config02.timebase_config.server)
+print(
+    "===================================================================================================="
+)
+config03 = load_config_3("test.yml")
 pprint(config03)
+print(config03.test1.test11.aaa)
+print(
+    "===================================================================================================="
+)
 
 #######################
-
-from configuration import Configuration
 
 
 # test_data = Configuration()
