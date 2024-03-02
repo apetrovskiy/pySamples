b40a317cec3102cb3fcd7a7ed6b8f3dc247712a0
Aleksandr Petrovskiy <uiautomation@hotmail.com>
2024-03-02 19:13:11 +0300

    it's enough for now

--- TheComfyChair/ClassGenerationFromYaml/init.sh
+++ TheComfyChair/ClassGenerationFromYaml/init.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-pipenv install pyyaml beartype python-dotenv --python 3.12
-pipenv install --dev black
+PYTHON_VER=3.12
+
+pipenv --venv
+pipenv --rm
+pipenv --venv
+rm Pipfile*
+
+pipenv install pyyaml beartype python-dotenv --pre --python ${PYTHON_VER}
+pipenv install --dev black --pre
--- TheComfyChair/ClassGenerationFromYaml/main.py
+++ TheComfyChair/ClassGenerationFromYaml/main.py
@@ -2,5 +2,5 @@ from typing import Protocol
 from company_loader import get_companies
 from protocols import Settings
 
-settings=protoc
+# settings = protoc
 get_companies(settings=Settings())
